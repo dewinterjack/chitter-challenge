6291f0c3268bc0fa4621a17d0a96fcd6546a4695
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-14 15:52:34 +0100

    RED feature: can see new peeps on home screen

--- chitter/spec/features/peeps_new_spec.rb
+++ chitter/spec/features/peeps_new_spec.rb
@@ -1,6 +1,9 @@
 feature 'adding new peep' do
-  scenario 'Can see created peeps on home page' do
-    
+  scenario 'Can see new peeps on home page' do
+      visit "/"
+      fill_in "new_peep", with: "Peep Peep!"
+      click_button "Peep"
+      expect(page).to have_content "Peep Peep!"
   end
 end
 
