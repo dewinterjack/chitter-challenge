102601f72eed9e94c65540fb274eab0c6ba0009d
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-14 12:58:46 +0100

    refactoring

--- chitter/app.rb
+++ chitter/app.rb
@@ -2,8 +2,9 @@ require 'sinatra'
 require './helpers/db_setup_helper.rb'
 require './models/peep'
 
+
 configure do
-  Bookmark.auto_upgrade!
+  Peep.auto_upgrade!
 end
 
 set :root, File.dirname(__FILE__)
